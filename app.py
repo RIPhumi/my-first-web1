59a15eb9826b47707d412ebdb0d1a11145a929c7
RIPhumi <helumilator@gmail.com>
2025-04-07 15:18:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,31 @@
-from flask import Flask, render_template, request, redirect, url_for
-import os
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
-
-# Password to access the IP log
+ip_list = []
 PASSWORD = "9554216787"
 
-# File to store IPs
-VISITOR_LOG = "visitor_ips.txt"
-
-def log_ip(ip_address):
-    # Avoid logging duplicates
-    if not os.path.exists(VISITOR_LOG):
-        with open(VISITOR_LOG, "w") as f:
-            f.write("")
-    with open(VISITOR_LOG, "r") as f:
-        logged_ips = f.read().splitlines()
-    if ip_address not in logged_ips:
-        with open(VISITOR_LOG, "a") as f:
-            f.write(ip_address + "\n")
+def get_client_ip():
+    if request.headers.get('X-Forwarded-For'):
+        ip = request.headers.get('X-Forwarded-For').split(',')[0]
+    else:
+        ip = request.remote_addr
+    return ip
 
 @app.route("/", methods=["GET"])
 def index():
-    ip = request.remote_addr
-    log_ip(ip)
     return render_template("index.html")
 
 @app.route("/login", methods=["POST"])
 def login():
-    entered_password = request.form.get("password")
-    if entered_password == PASSWORD:
-        with open(VISITOR_LOG, "r") as f:
-            ip_list = f.read().splitlines()
+    password = request.form.get("password")
+    if password == PASSWORD:
+        ip = get_client_ip()
+        if ip not in ip_list:
+            ip_list.append(ip)
         return render_template("dashboard.html", ips=ip_list)
     else:
-        return render_template("index.html", error="Incorrect password!")
+        return "Incorrect password", 403
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=False)
+
